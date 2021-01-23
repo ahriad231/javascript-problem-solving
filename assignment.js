667dc9cbd63de1aa27198ec326ccf4adc8e5772a
AH RIAD <ahriad231@gmail.com>
2021-01-23 10:10:28 +0600

    two problem solve

---
+++ assignment.js
@@ -0,0 +1,20 @@
+Problem:1
+
+// find value kilometer to meter 
+
+function kilometerToMeter(kilometer){
+var meter=kilometer*1000;
+return meter;
+}
+
+Problem:2
+
+// find totalPrice with budgetCalculator function
+function budgetCalculator(watch,mobile,laptop){
+    var watchPrice=watch*50;
+    var mobilePrice=mobile*100;
+    var laptopPrice=laptop*500;
+    var totalPrice=(watchPrice+mobilePrice+laptopPrice);
+    return totalPrice;
+}
+Problem:3
